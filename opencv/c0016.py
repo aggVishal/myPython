8151f2131d7d526bfbb0ba734b27cfa52ddadff5
Vishalagg901 <aggarwalvishal901@gmail.com>
2020-06-13 14:13:28 +0530

    update

--- opencv/c0016.py
+++ opencv/c0016.py
@@ -4,21 +4,22 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img=cv2.imread('sudoku.png',cv2.IMREAD_GRAYSCALE)
+img=cv2.imread('messi5.jpg',cv2.IMREAD_GRAYSCALE)
 
 lap=cv2.Laplacian(img,cv2.CV_64F,ksize=3) #laplacian Gradient
 lap=np.uint8(np.absolute(lap))
 
 sobelX=cv2.Sobel(img,cv2.CV_64F,1,0)
 sobelY=cv2.Sobel(img,cv2.CV_64F,0,1)
+canny=cv2.Canny(img,100,200) # canny edge detection
 
 sobelX=np.uint8(np.absolute(sobelX))
 sobelY=np.uint8(np.absolute(sobelY))
 
 mergeSobel=cv2.bitwise_or(sobelX,sobelY)
 
-titles=['image','Laplacian Gradient','sobelX','sobelY','mergeSobel']
-images=[img,lap,sobelX,sobelY,mergeSobel]
+titles=['image','Laplacian Gradient','sobelX','sobelY','mergeSobel','canny']
+images=[img,lap,sobelX,sobelY,mergeSobel,canny]
 for i in range(len(images)):
     plt.subplot(2,3,i+1) , plt.imshow(images[i],'gray')
     plt.title(titles[i])
--- opencv/c0017.py
+++ opencv/c0017.py
@@ -1,16 +1,17 @@
-#Canny Edge Detector
+# Find and Draw Contours
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img=cv2.imread('sample.jpg')
-img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+img=cv2.imread('logo.png')
+imgray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 
-titles=['image']
-images=[img]
+
+titles=['image','imgray']
+images=[img,imgray]
 for i in range(len(images)):
-    plt.subplot(2,3,i+1) , plt.imshow(images[i],'gray')
+    plt.subplot(2,1,i+1) , plt.imshow(images[i],'gray')
     plt.title(titles[i])
     plt.xticks([]),plt.yticks([])
 
